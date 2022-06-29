2e3707fcf11119a9e58deb5cc99e6ea80f5bd601
Giray Hakan <47675791+gryhkn@users.noreply.github.com>
2022-06-29 08:26:39 +0300

    Update MyEpicNFT.sol

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.1;
 
-// We first import some OpenZeppelin Contracts.
+// We first import some OpenZeppelin Contracts
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
