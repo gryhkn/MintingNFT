7f721a948657fc236b8473e90039c498643abbef
Giray Hakan <47675791+gryhkn@users.noreply.github.com>
2022-06-29 08:27:10 +0300

    Update MyEpicNFT.sol

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
 // We inherit the contract we imported. This means we'll have access
-// to the inherited contract's methods.
+// to the inherited contract's methods
 contract MyEpicNFT is ERC721URIStorage {
   // Magic given to us by OpenZeppelin to help us keep track of tokenIds
   using Counters for Counters.Counter;
@@ -22,14 +22,14 @@ contract MyEpicNFT is ERC721URIStorage {
      // Get the current tokenId, this starts at 0.
     uint256 newItemId = _tokenIds.current();
 
-     // Actually mint the NFT to the sender using msg.sender.
+     // Actually mint the NFT to the sender using msg.sender
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data
     _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiVVBORVJTIiwKICAgICJkZXNjcmlwdGlvbiI6ICJVUE5FUlMgTkZUIGNvbGxlY3Rpb24iLAogICAgImltYWdlIjogImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1ZWQk9SVkpUUEM5MFpYaDBQZ284TDNOMlp6ND0iCn0=");
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
-    // Increment the counter for when the next NFT is minted.
+    // Increment the counter for when the next NFT is minted
     _tokenIds.increment();
   }
 }
